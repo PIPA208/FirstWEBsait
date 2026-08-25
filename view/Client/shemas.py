fa199a864271ed69059c91f077939b0837376899
PIPA208 <xnxdelik208@mail.ru>
2026-08-25 12:57:30 +0300

    end created view.Client.shemas

--- view/Client/shemas.py
+++ view/Client/shemas.py
@@ -1,1 +1,12 @@
 #TODO
+from pydantic import BaseModel
+# SHEMAS
+class ClientBase(BaseModel):
+    name : str
+    age : int
+class ClientCreate(ClientBase):
+    '''
+    this class created a new client
+    '''
+class Client(ClientBase):
+    id : int
